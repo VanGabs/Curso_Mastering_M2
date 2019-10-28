35923f1d1e2278e2e7e28e919e1b1659e6261127
Gabriele <gabriele.santos.silva@hotmail.com>
2019-10-28 15:53:00 -0300

    Modulo feito sozinha

--- app/code/Mastering/SampleModule/Console/Command/AddItem.php
+++ app/code/Mastering/SampleModule/Console/Command/AddItem.php
@@ -2,12 +2,12 @@
 
 namespace Mastering\SampleModule\Console\Command;
 
+use Magento\Framework\Console\Cli;
+use Mastering\SampleModule\Model\ItemFactory;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Mastering\SampleModule\Model\ItemFactory;
-use Magento\Framework\Console\Cli;
 
 class AddItem extends Command
 {
@@ -15,7 +15,6 @@ class AddItem extends Command
     const INPUT_KEY_DESCRIPTION = 'description';
 
     private $itemFactory;
-   
 
     public function __construct(ItemFactory $itemFactory)
     {
